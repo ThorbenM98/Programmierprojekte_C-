de00d98888d4cdd43012ea9104102087c1ffdbfb
ThorbenM98 <Thorben.moe@gmail.com>
2025-04-03 12:03:00 +0200

    test

--- C/Test/Test/Test.cpp
+++ C/Test/Test/Test.cpp
@@ -6,16 +6,16 @@ using namespace std;
 int date;
 int day;
 string month;
+#define leer cout << "\n";
+
 
 int main() {
-    
+
     //alternativ auch char[] name
     string month = "April";
     int day = 3;
     int date = 2025;
-    cout <<"year: " + to_string(date) + " month: " + month +" day: " + to_string(day);
+    cout << "year: " + to_string(date) + " month: " + month + " day: " + to_string(day);
+
 
     return 0;
-}
-
-
